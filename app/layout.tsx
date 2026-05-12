b3a2bcb16279bdc5f69e9369f48da2395f5db145
marianne-yo <mbalen39@gmail.com>
2026-05-12 15:04:50 +0800

    updated the app font

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} antialiased`} suppressHydrationWarning>
+      <body className={`${poppins.variable} font-sans antialiased`} suppressHydrationWarning>
         {children}
       </body>
     </html>
